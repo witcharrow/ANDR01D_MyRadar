1187fce86cea859824a0d348f1d801162a131428
WitchArroW <alejandro.marijuan@googlemail.com>
2014-01-29 23:14:29 +0100

    Primera versión. Limpia.

---
+++ A_UD8_EJF_MyRadar/src/es/alexmj/myradar_miradarpersonal/RadarActivity.java
@@ -0,0 +1,22 @@
+package es.alexmj.myradar_miradarpersonal;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class RadarActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_radar);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.radar, menu);
+		return true;
+	}
+
+}
